1d31aadf8eb221bd8722aabe23d609fc4cb9b98a
Luiz <lg.luizh@gmail.com>
2019-11-14 09:31:50 -0300

    Criado o tema 'light'

---
+++ src/config/themes/light/styles.js
@@ -0,0 +1,11 @@
+const themeLight = {
+  bgcolor: '#f8f8f2',
+  border: 'none',
+  color: '#353535',
+  borderItemActive: '2px dotted #44475a',
+  playedCircleColor: '#3bc35e',
+  numberBorderColor: '#44475a',
+  timeBgColor: '#44475a'
+}
+
+export default themeLight
