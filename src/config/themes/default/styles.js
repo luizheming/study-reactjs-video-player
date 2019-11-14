7f9288a83810bcac26a562f3fd0bd9283db9ef5c
Luiz <lg.luizh@gmail.com>
2019-11-14 09:31:20 -0300

    Configuração do tema 'default'

---
+++ src/config/themes/default/styles.js
@@ -0,0 +1,11 @@
+const themeDefault = {
+  bgcolor: '#44475a',
+  border: 'none',
+  color: '#fff',
+  borderItemActive: '2px dotted #6272a4',
+  playedCircleColor: '#50fa7b',
+  numberBorderColor: '#6272a4',
+  timeBgColor: '#6272a4'
+}
+
+export default themeDefault
